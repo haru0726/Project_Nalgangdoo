b47f1da2dd23a210d0298386416e126bc8798969
sj98h <sj98h@naver.com>
2024-09-14 14:25:52 +0900

    [update] nodemon 환경

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
-app.use("/api", []);
+// app.use("/api", []);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
