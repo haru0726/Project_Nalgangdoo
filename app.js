83d74e8b2526f85badb9611a34a52686d6793702
sj98h <sj98h@naver.com>
2024-09-18 16:26:42 +0900

    [update] 회원가입 / 로그인 API 추가
    추가로 각 api 버전 관리를 위한 코드 컨벤션 제안 드립니다

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express";
+import UserRouter from "./src/routes/user.router.js";
 
 const app = express();
 const port = 3000;
 app.use(express.json());
 
-// app.use("/api", []);
+app.use("/api", [UserRouter]);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
