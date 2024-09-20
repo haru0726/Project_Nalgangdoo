f9732b6f2f41928755dc54ed012557c8cb2fbdbe
sj98h <sj98h@naver.com>
2024-09-20 15:50:06 +0900

    [update] 선수 생성 API 업데이트
    관리자 계정 인가 처리했습니다. 아직 관리자 계정을 만들 수 있는 api가 없으니 추가가 필요합니다.

--- src/routes/character.router.js
+++ src/routes/character.router.js
@@ -17,6 +17,25 @@ router.post("/character-data", authMiddleware, async (req, res, next) => {
     const { name, speed, goalDetermination, shootPower, defense, stamina } =
       req.body;
 
+    // 인증 미들웨어에서 받은 유저 아이디
+    const { userId } = req.user;
+
+    // 유저 아이디 조회
+    const account = await prisma.account.findUnique({
+      where: {
+        userId: userId,
+      },
+    });
+    if (!account) {
+      return res.status(404).json({ message: "존재하지 않는 계정입니다." });
+    }
+
+    // 관리자 인가
+    if (!account.super) {
+      return res.status(403).json({ message: "관리자 권한이 필요합니다." });
+    }
+
+    // 필수 필드
     const requiredFields = [
       name,
       speed,
@@ -25,21 +44,29 @@ router.post("/character-data", authMiddleware, async (req, res, next) => {
       defense,
       stamina,
     ];
-    const isExistCharacterName = await prisma.account.findUnique({
+    // 기존 캐릭터 이름 조회
+    const isExistCharacterName = await prisma.character.findUnique({
       where: { name },
     });
 
     // 유효성 검사
     const missingFields = requiredFields.filter((field) => !field);
-    if (missingFields.length) {
-      return res.status(400).json({
-        message: "다음 필드가 누락되었습니다: " + missingFields.join(", "),
-      });
+    if (missingFields.length > 0) {
+      return res.status(400).json({ message: "필드가 누락되었습니다." });
     }
     if (isExistCharacterName) {
       return res.status(409).json({ message: "이미 존재하는 이름입니다." });
     }
+
+    // character 테이블에 요청 받은 캐릭터 추가
+    await prisma.character.create({
+      data: { name, speed, goalDetermination, shootPower, defense, stamina },
+    });
+
+    return res.status(201).json({ message: "캐릭터가 생성되었습니다." });
   } catch (err) {
     console.log(err);
   }
 });
+
+export default router;
